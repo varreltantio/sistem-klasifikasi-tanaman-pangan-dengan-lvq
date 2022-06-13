6215e23bf515ae92f8ec2c11a1c0e3c0bf89d1ff
varreldt <varreldtantio@gmail.com>
2022-06-13 11:08:38 +0700

    first commit

---
+++ app/main.py
@@ -0,0 +1,42 @@
+from flask import Flask, render_template, jsonify, request
+import LVQ
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/rekomendasi')
+def rekomendasi():
+    return render_template('rekomendasi.html')
+
+@app.route('/result')
+def result():
+    return render_template('result.html')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    try:
+        json_ = request.get_json()
+
+        data = [
+            json_['N'],
+            json_['P'],
+            json_['K'],
+            json_['temperature'],
+            json_['humidity'],
+            json_['ph'],
+            json_['rainfall']
+        ]
+
+        lvq = LVQ.model()
+        prediction = lvq.predict(data)
+
+        return jsonify({'prediction': str(prediction)})
+    except ValueError:
+        return jsonify({'error': ValueError})
+   
+@app.route('/team')
+def team():
+    return render_template('team.html')
